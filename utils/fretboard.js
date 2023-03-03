1be24bf97035bbe1b871442055bfd595b838f494
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2023-03-03 22:24:08 +0000

    refactored to pass addNoteToFretBoard beat argument. tests changed.

--- utils/fretboard.js
+++ utils/fretboard.js
@@ -2,14 +2,13 @@ const { findString } = require("./strings");
 const { stringsAndNotes } = require("./notes");
 
 const printFretboard = (note) => {
+  let beat = 1;
   let fretboardTemplate = ["e ||", "B ||", "G ||", "D ||", "A ||", "E ||"];
-  const fretboard = addNoteToFretboard(note, fretboardTemplate);
+  const fretboard = addNoteToFretboard(note, fretboardTemplate, beat);
   for (let i = 0; i < fretboard.length; i++) {
     console.log(fretboard[i] + "\n");
   }
 };
-
-let beat = 1;
 
 // --NOTES FOR NEXT TIME:
 // Make the printFretboard function a loop that itirates over an array of notes.
@@ -18,7 +17,17 @@ let beat = 1;
 // Also, edit code as a "--" is needed on each string that isn't being given a note.
 //"---" if the note is double digits.
 
-const addNoteToFretboard = (note, fretboard) => {
+//  --Rough Plan:
+// for (let i = 0; i < notes.length; i++) {
+//   // loop through array
+
+//   if ((i + 1) % 8 === 0) {
+//     // check if index is a multiple of 8
+//     // run extra line of code here
+//   }
+// }
+
+const addNoteToFretboard = (note, fretboard, beat) => {
   const stringIndex = findString(note);
   // Get the correct guitar string based off the note and return a split copy
   let currentString = fretboard[stringIndex];
@@ -26,7 +35,8 @@ const addNoteToFretboard = (note, fretboard) => {
   // Transcribe the note depending on the string
   let noteToAdd = note - stringsAndNotes[stringIndex][0];
   // Add note to our guitar string copy and convert back to string
-  splitString.splice(beat + 2, 0, `-${noteToAdd.toString()}`);
+  let position = beat + 2;
+  splitString.splice(position, 0, `-${noteToAdd.toString()}`);
   currentString = splitString.join("");
   // Add new guitar string to our fretboard
   fretboard[stringIndex] = currentString;
